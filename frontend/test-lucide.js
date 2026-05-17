90f1111003722f21d872c219bc1626b451553b9d
YashVarshney005 <dr.infinity000@gmail.com>
2026-05-18 03:42:13 +0530

    fixed

--- frontend/test-lucide.js
+++
@@ -1,3 +0,0 @@
-import * as lucide from 'lucide-react';
-console.log(Object.keys(lucide).filter(k => k.toLowerCase().includes('git')));
-console.log(Object.keys(lucide).filter(k => k.toLowerCase().includes('link')));
--- frontend/test-lucide2.js
+++
@@ -1,5 +0,0 @@
-import * as lucide from 'lucide-react';
-import fs from 'fs';
-const gitIcons = Object.keys(lucide).filter(k => k.toLowerCase().includes('git'));
-const linkIcons = Object.keys(lucide).filter(k => k.toLowerCase().includes('link'));
-fs.writeFileSync('output-utf8.txt', JSON.stringify({gitIcons, linkIcons}, null, 2), 'utf8');
